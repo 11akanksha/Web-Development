692f0b624d9ab3d5160e9140516c558e38bbd891
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-08-15 17:11:02 +0530

    My first Java Script!

---
+++ j1.js
@@ -0,0 +1,35 @@
+// function checkDriverAge1() {
+//     var age = prompt("What is your age?");
+
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+// var checkDriverAge2 = function () {
+//     checkDriverAge1();
+// }
+
+// function checkDriverAge(age) {
+//     if (Number(age) < 18) {
+//         alert("Sorry, you are too yound to drive this car. Powering off");
+//     } else if (Number(age) > 18) {
+//         alert("Powering On. Enjoy the ride!");
+//     } else if (Number(age) === 18) {
+//         alert("Congratulations on your first year of driving. Enjoy the ride!");
+//     }
+// }
+
+var userDetails = {
+    username: "Stephen",
+    password: "secret"
+};
+
+var userTimes = {};
+
+var database = [userDetails];
+var newsfeed = [userTimes, userTimes, userTimes];
