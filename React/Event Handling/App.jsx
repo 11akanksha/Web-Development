ebc98bdd70f3ea224d7dc32c5e447bfd94e58130
11akanksha <64818557+11akanksha@users.noreply.github.com>
2021-10-17 22:16:46 +0530

    Create App.jsx

---
+++ React/Event Handling/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [headingText, setHeadingText] = useState("Hello");
+  const [isHovered, setIsHovered] = useState(false);
+
+  function handleClick() {
+    setHeadingText("Submitted");
+  }
+
+  function handleHover() {
+    setIsHovered(true);
+  }
+
+  function handleMouseOut() {
+    setIsHovered(false);
+  }
+
+  return (
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        style={{ backgroundColor: isHovered ? "black" : "white" }}
+        onClick={handleClick}
+        onMouseOver={handleHover}
+        onMouseOut={handleMouseOut}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default App;
