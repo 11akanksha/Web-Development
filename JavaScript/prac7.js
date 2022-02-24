efbe1cef93e2fe779cd24883e92671933e5b2eb2
11akanksha <64818557+11akanksha@users.noreply.github.com>
2022-02-24 23:26:07 +0530

    Create prac7.js

---
+++ JavaScript/prac7.js
@@ -0,0 +1,18 @@
+for (var i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 0);
+    console.log('hello');
+}
+
+//O/p:
+hello
+hello
+hello
+hello
+hello
+5
+5
+5
+5
+5
